f8f207ab3c495a4ea91ea21f331e68ef80f8e73c
Arseniy Tashoyan <tashoyan@gmail.com>
2018-11-28 17:36:05 +0300

    Dummy test

--- topology/src/test/scala/com/github/tashoyan/telecom/topology/DummyGraphTest.scala
+++ topology/src/test/scala/com/github/tashoyan/telecom/topology/DummyGraphTest.scala
@@ -2,6 +2,8 @@ package com.github.tashoyan.telecom.topology
 
 import com.github.tashoyan.test.SparkTestHarness
 import org.scalatest.FunSuite
+import org.apache.spark.sql.functions._
+import org.graphframes.GraphFrame
 
 class DummyGraphTest extends FunSuite with SparkTestHarness {
 
@@ -11,6 +13,17 @@ class DummyGraphTest extends FunSuite with SparkTestHarness {
       .toString
     val topology = spark.read.parquet(topologyFile)
     topology.show()
+    println(s"Distinct stations: ${topology.select("station").distinct().count()}")
+    println(s"Distinct controllers: ${topology.select("controller").distinct().count()}")
+
+    val graph = GraphFrame.fromEdges(topology.select(col("station") as "src", col("controller") as "dst"))
+    graph.triplets.show(false)
+
+    spark.sparkContext.setCheckpointDir("checkpoint")
+    val connectedComponents = graph.connectedComponents.run()
+    connectedComponents.show(false)
+    println(s"Distinct ids: ${connectedComponents.select("id").distinct().count()}")
+    println(s"Distinct components: ${connectedComponents.select("component").distinct().count()}")
   }
 
 }

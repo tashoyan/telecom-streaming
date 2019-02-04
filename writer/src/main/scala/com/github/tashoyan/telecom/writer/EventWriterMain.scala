ad36940f31dafff694e971e1eb43accc0be3e759
Arseniy Tashoyan <tashoyan@gmail.com>
2019-02-04 13:29:54 +0300

    Refactor: Extract function loadEvents

--- writer/src/main/scala/com/github/tashoyan/telecom/writer/EventWriterMain.scala
+++ writer/src/main/scala/com/github/tashoyan/telecom/writer/EventWriterMain.scala
@@ -5,10 +5,10 @@ import java.sql.Timestamp
 import com.github.tashoyan.telecom.event.Event._
 import com.github.tashoyan.telecom.spark.DataFrames.RichDataFrame
 import com.github.tashoyan.telecom.util.Timestamps._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.OutputMode
-import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.{StringType, StructType}
 
 object EventWriterMain extends EventWriterArgParser {
   private val spark = SparkSession.builder()
@@ -29,6 +29,27 @@ object EventWriterMain extends EventWriterArgParser {
       .parquet(config.schemaFile)
       .schema
 
+    val events = loadEvents(config, schema)
+
+    val yearMonthColumn = "year_month"
+    val yearMonthUdf = udf { origEventTime: Timestamp => yearMonth(origEventTime) }
+    val outputEvents = events
+      .repartition(col(siteIdColumn))
+      .withColumn(yearMonthColumn, yearMonthUdf(col(timestampColumn)))
+
+    val query = outputEvents
+      .writeStream
+      .outputMode(OutputMode.Append())
+      .queryName(getClass.getSimpleName)
+      .partitionBy(siteIdColumn, yearMonthColumn)
+      .format("parquet")
+      .option("path", config.outputDir)
+      .option("checkpointLocation", config.checkpointDir)
+      .start()
+    query.awaitTermination()
+  }
+
+  private def loadEvents(config: EventWriterConfig, schema: StructType): DataFrame = {
     /*
     TODO Why only one Kafka consumer?
     https://stackoverflow.com/questions/53605061/spark-structured-streaming-kafka-source-how-many-consumers
@@ -42,8 +63,6 @@ object EventWriterMain extends EventWriterArgParser {
       .load()
 
     val jsonColumn = "json_value"
-    val yearMonthColumn = "year_month"
-    val yearMonthUdf = udf { origEventTime: Timestamp => yearMonth(origEventTime) }
     val events = kafkaEvents
       .select(col("value") cast StringType as jsonColumn)
       .parseJsonColumn(jsonColumn, schema)
@@ -54,19 +73,7 @@ object EventWriterMain extends EventWriterArgParser {
       */
       .withWatermark(timestampColumn, s"${config.watermarkIntervalSec} seconds")
       .dropDuplicates(timestampColumn, siteIdColumn)
-      .repartition(col(siteIdColumn))
-      .withColumn(yearMonthColumn, yearMonthUdf(col(timestampColumn)))
-
-    val query = events
-      .writeStream
-      .outputMode(OutputMode.Append())
-      .queryName(getClass.getSimpleName)
-      .partitionBy(siteIdColumn, yearMonthColumn)
-      .format("parquet")
-      .option("path", config.outputDir)
-      .option("checkpointLocation", config.checkpointDir)
-      .start()
-    query.awaitTermination()
+    events
   }
 
 }

fe3ec17858e5a4e9e8f628147028779c59de29e2
Arseniy Tashoyan <tashoyan@gmail.com>
2019-02-11 18:52:08 +0300

    Migrate local predictor implementation to FireAlarmStateFunction

--- predictor-spark/src/main/scala/com/github/tashoyan/telecom/predictor/SparkPredictorLocal.scala
+++ predictor-spark/src/main/scala/com/github/tashoyan/telecom/predictor/SparkPredictorLocal.scala
@@ -3,19 +3,17 @@ package com.github.tashoyan.telecom.predictor
 import java.sql.Timestamp
 import java.util.concurrent.TimeUnit
 
-import com.github.tashoyan.telecom.event.Event
 import com.github.tashoyan.telecom.event.Event._
 import com.github.tashoyan.telecom.event.SparkEventAdapter.EventDataFrame
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, OutputMode}
+import org.apache.spark.sql.streaming.{GroupStateTimeout, OutputMode}
 import org.apache.spark.sql.types.LongType
 
-//scalastyle:off
 object SparkPredictorLocal {
   private val port: Long = 9999
   private val watermarkIntervalSec = 20L
-  private val problemTimeoutMillis = TimeUnit.SECONDS.toMillis(10L)
+  private val problemTimeoutSec = 10L
 
   def main(args: Array[String]): Unit = {
     implicit val spark: SparkSession = SparkSession.builder()
@@ -46,72 +44,11 @@ object SparkPredictorLocal {
       .withColumn(infoColumn, infoCol)
       .asEvents
 
-    def isHeat(event: Event): Boolean =
-      event.info != null &&
-        event.info.toLowerCase.contains("heat")
-
-    def isSmoke(event: Event): Boolean =
-      event.info != null &&
-        event.info.toLowerCase.contains("smoke")
-
-    def dummyAlarmFunction: (Long, Iterator[Event], GroupState[DummyState1]) => Iterator[DummyAlarm1] = {
-      (siteId, events, state) =>
-        if (state.hasTimedOut) {
-          println(s"TIMED OUT STATE on $siteId")
-          val heatTimestamp = state.getOption.map(_.heatTimestamp)
-          println(s" -- heatTimestamp: $heatTimestamp")
-          println(s" -- now: ${new Timestamp(System.currentTimeMillis())}")
-          state.remove()
-          Iterator.empty
-        } else if (state.exists) {
-          println(s"EXISTING STATE on $siteId")
-          val heatTimestamp = state.get.heatTimestamp
-          println(s" -- heatTimestamp: $heatTimestamp")
-          val smokeTimestamp = events.toStream
-            .find(isSmoke)
-            .map(_.timestamp)
-          val timeoutTimestamp = heatTimestamp.getTime + problemTimeoutMillis
-          state.setTimeoutTimestamp(timeoutTimestamp)
-
-          if (smokeTimestamp.isDefined) {
-            println(s" -- smokeTimestamp: $smokeTimestamp")
-            val smokeTs = smokeTimestamp.get
-            if (smokeTs.getTime - heatTimestamp.getTime > 0 &&
-              smokeTs.getTime - heatTimestamp.getTime <= problemTimeoutMillis) {
-              val alarm = DummyAlarm1(siteId, heatTimestamp, smokeTs, s"Fire on site $siteId")
-              state.remove()
-              Iterator(alarm)
-            } else {
-              println(" -- smoke is too late")
-              state.remove()
-              Iterator.empty
-            }
-          } else {
-            println(" -- no smoke yet")
-            Iterator.empty
-          }
-        } else {
-          println(s"NEW STATE on $siteId")
-          val heatTimestamp = events.toStream
-            .find(isHeat)
-            .map(_.timestamp)
-          heatTimestamp.foreach { heatTs =>
-            println(s" -- heatTimestamp: $heatTs")
-            val newState = DummyState1(siteId, heatTs)
-            state.update(newState)
-            val timeoutTimestamp = heatTs.getTime + problemTimeoutMillis
-            state.setTimeoutTimestamp(timeoutTimestamp)
-            println(s" -- timeoutTimestamp: ${new Timestamp(timeoutTimestamp)}")
-            println(s" -- watermark timeout timestamp: ${new Timestamp(timeoutTimestamp + TimeUnit.SECONDS.toMillis(watermarkIntervalSec))}")
-          }
-          Iterator.empty
-        }
-    }
-
+    val alarmStateFunction = new FireAlarmStateFunction(TimeUnit.SECONDS.toMillis(problemTimeoutSec))
     val alarms = events
       .withWatermark(timestampColumn, s"$watermarkIntervalSec seconds")
       .groupByKey(_.siteId)
-      .flatMapGroupsWithState(OutputMode.Update(), GroupStateTimeout.EventTimeTimeout())(dummyAlarmFunction)
+      .flatMapGroupsWithState(OutputMode.Update(), GroupStateTimeout.EventTimeTimeout())(alarmStateFunction.updateAlarmState)
 
     val query = alarms.writeStream
       .outputMode(OutputMode.Update())

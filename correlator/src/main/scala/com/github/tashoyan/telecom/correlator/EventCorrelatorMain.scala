e7271a2fd748f3761272c256b0fc8ea5db8ee503
Arseniy Tashoyan <tashoyan@gmail.com>
2019-02-27 11:46:31 +0300

    Correlator sends typed alarms

--- correlator/src/main/scala/com/github/tashoyan/telecom/correlator/EventCorrelatorMain.scala
+++ correlator/src/main/scala/com/github/tashoyan/telecom/correlator/EventCorrelatorMain.scala
@@ -1,14 +1,14 @@
 package com.github.tashoyan.telecom.correlator
 
 import com.github.tashoyan.telecom.event.Event._
-import com.github.tashoyan.telecom.event.{DefaultEventDeduplicator, KafkaEventReceiver}
-import com.github.tashoyan.telecom.spark.DataFrames.RichDataFrame
+import com.github.tashoyan.telecom.event.{Alarm, DefaultEventDeduplicator, KafkaEventReceiver}
+import com.github.tashoyan.telecom.spark.DataFrames.RichDataset
 import com.github.tashoyan.telecom.spark.KafkaStream._
 import com.github.tashoyan.telecom.topology.Topology._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, StreamingQuery}
 import org.apache.spark.sql.types.StringType
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Dataset, SparkSession}
 
 object EventCorrelatorMain extends EventCorrelatorArgParser {
 
@@ -26,6 +26,7 @@ object EventCorrelatorMain extends EventCorrelatorArgParser {
       .getOrCreate()
     spark.sparkContext
       .setLogLevel("WARN")
+    import spark.implicits._
 
     val topology = spark.read
       .parquet(config.topologyFile)
@@ -40,18 +41,13 @@ object EventCorrelatorMain extends EventCorrelatorArgParser {
     val events = eventDeduplicator.deduplicateEvents(kafkaEvents)
 
     val affectedStationCounts = events
-      /*
-      Inner join - dropping events with unknown stations.
-      Possible optimization: broadcast join.
-      */
+      /* Inner join - dropping events with unknown stations. Possible optimization: broadcast join. */
       .join(topology, col(siteIdColumn) === col(stationColumn), "inner")
       .groupBy(
         window(col(timestampColumn), s"${config.windowSizeSec} seconds", s"${config.windowShiftSec} seconds"),
         col(controllerColumn)
       )
-      /*
-      Workaround: countDistinct() is unsupported for streaming data sets (Spark 2.4.0)
-      */
+      /* Workaround: countDistinct() is unsupported for streaming data sets (Spark 2.4.0) */
       .agg(collect_set(siteIdColumn) as "affected_stations")
       .withColumn("affected_station_count", size(col("affected_stations")))
       .drop("affected_stations")
@@ -59,13 +55,20 @@ object EventCorrelatorMain extends EventCorrelatorArgParser {
     val controllerAlarms = affectedStationCounts
       .join(totalStationCounts, Seq(controllerColumn), "inner")
       .where(col("affected_station_count") === col("total_station_count"))
+      .select(
+        col("window.start") as Alarm.timestampColumn,
+        col(controllerColumn) as Alarm.objectIdColumn,
+        lit("CRITICAL") as Alarm.severityColumn,
+        lit("Communication lost with the controller") as Alarm.infoColumn
+      )
+      .as[Alarm]
 
     val query = createAndStartAlarmQuery(config, controllerAlarms)
     query.awaitTermination()
   }
 
   //TODO Makes sense to extract alarm stream to a separate class
-  private def createAndStartAlarmQuery(config: EventCorrelatorConfig, alarms: DataFrame): StreamingQuery = {
+  private def createAndStartAlarmQuery(config: EventCorrelatorConfig, alarms: Dataset[Alarm]): StreamingQuery = {
     val kafkaAlarms = alarms
       .withJsonColumn(valueColumn)
       .withColumn(keyColumn, col(controllerColumn) cast StringType)

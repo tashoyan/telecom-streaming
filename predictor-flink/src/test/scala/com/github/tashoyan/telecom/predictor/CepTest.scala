b5a9fa5b6391e166ca92750f16d68146e8433185
Arseniy Tashoyan <tashoyan@gmail.com>
2019-03-26 18:43:31 +0300

    Prototype for CepFirePredictor

--- predictor-flink/src/test/scala/com/github/tashoyan/telecom/predictor/CepTest.scala
+++ predictor-flink/src/test/scala/com/github/tashoyan/telecom/predictor/CepTest.scala
@@ -1,6 +1,7 @@
 package com.github.tashoyan.telecom.predictor
 
 import org.apache.flink.cep.scala.CEP
+import org.apache.flink.cep.scala.conditions.Context
 import org.apache.flink.cep.scala.pattern.Pattern
 import org.apache.flink.streaming.api.scala.{DataStream, _}
 import org.apache.flink.test.util.AbstractTestBase
@@ -35,4 +36,35 @@ class CepTest extends AbstractTestBase with JUnitSuiteLike {
     ()
   }
 
+  @Test def fireAlarm(): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val problemTimeout = 5L
+    val input: DataStream[Evt] = env.fromCollection(Seq(
+      Evt(1L, "heat"),
+      Evt(2L, "heat"),
+      Evt(3L, "smoke")
+    ))
+
+    val pattern = Pattern.begin[Evt]("heat").where(_.eventType == "heat")
+      .followedBy("smoke").where { (event: Evt, ctx: Context[Evt]) =>
+        lazy val latestHeat = ctx.getEventsForPattern("heat")
+          .toSeq
+          .sortBy(-_.ts)
+          .headOption
+        event.eventType == "smoke" && latestHeat.exists(event.ts - _.ts <= problemTimeout)
+      }
+    val patternStream = CEP.pattern(input, pattern)
+    val alarms: DataStream[String] = patternStream.select { foundPattern: Map[String, Iterable[Evt]] =>
+      val heat = foundPattern("heat").head
+      val smoke = foundPattern("smoke").head
+      s"Fire alarm: heat: $heat, smoke: $smoke"
+    }
+    alarms.print()
+
+    env.execute()
+    ()
+  }
+
 }
+
+case class Evt(ts: Long, eventType: String) extends Serializable

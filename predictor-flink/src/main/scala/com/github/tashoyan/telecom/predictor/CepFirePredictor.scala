ee0ede2b14ea70cdd5f50506dda023286460a7bd
Arseniy Tashoyan <tashoyan@gmail.com>
2019-04-02 04:27:33 +0300

    Fix: Handle heat and smoke events having same timestamp. Previous implementation had a mistake that led to discarded events.

--- predictor-flink/src/main/scala/com/github/tashoyan/telecom/predictor/CepFirePredictor.scala
+++ predictor-flink/src/main/scala/com/github/tashoyan/telecom/predictor/CepFirePredictor.scala
@@ -2,9 +2,9 @@ package com.github.tashoyan.telecom.predictor
 
 import com.github.tashoyan.telecom.event.FireAlarmUtil._
 import com.github.tashoyan.telecom.event.{Alarm, Event}
+import org.apache.flink.cep.EventComparator
 import org.apache.flink.cep.nfa.aftermatch.AfterMatchSkipStrategy
 import org.apache.flink.cep.scala.CEP
-import org.apache.flink.cep.scala.conditions.Context
 import org.apache.flink.cep.scala.pattern.Pattern
 import org.apache.flink.streaming.api.scala.{DataStream, _}
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -16,6 +16,21 @@ class CepFirePredictor(
     eventOutOfOrdernessMillis: Long
 ) extends AbstractFlinkFirePredictor(eventOutOfOrdernessMillis) {
 
+  private object FireCandidateComparator extends EventComparator[Event] {
+    override def compare(e1: Event, e2: Event): Int = {
+      val byTimestamp = e1.timestamp compareTo e2.timestamp
+      if (byTimestamp != 0)
+        byTimestamp
+      else
+        /*
+      For heat and smoke events having same timestamp:
+      put the smoke event first, this disables pattern trigger.
+      We count only smoke events strictly after heat events.
+      */
+        e1.isHeat compareTo e2.isHeat
+    }
+  }
+
   override def predictAlarms(events: DataStream[Event]): DataStream[Alarm] = {
     val fireCandidates = events
       .filter(e => isFireCandidate(e))
@@ -25,18 +40,10 @@ class CepFirePredictor(
     val heatPattern = "heat"
     val smokePattern = "smoke"
     val pattern = Pattern.begin[Event](heatPattern, AfterMatchSkipStrategy.skipPastLastEvent()).where(_.isHeat).oneOrMore
-      .next(smokePattern).where { (event: Event, ctx: Context[Event]) =>
-        /*
-        Smoke event should be strictly after heat event. Maybe minimal problem timeout is needed.
-        CEP sorts events by timestamp in ascending order:
-         https://ci.apache.org/projects/flink/flink-docs-release-1.7/dev/libs/cep.html#handling-lateness-in-event-time
-        */
-        val firstHeat = ctx.getEventsForPattern(heatPattern).head
-        event.isSmoke && event.timestamp != firstHeat.timestamp
-      }
+      .next(smokePattern).where(_.isSmoke)
       .within(Time.milliseconds(problemTimeoutMillis))
 
-    val patternStream = CEP.pattern(fireCandidates, pattern)
+    val patternStream = CEP.pattern(fireCandidates, pattern, FireCandidateComparator)
     val alarms: DataStream[Alarm] = patternStream
       .select { foundPattern: Map[String, Iterable[Event]] =>
         val heat = foundPattern(heatPattern).head
